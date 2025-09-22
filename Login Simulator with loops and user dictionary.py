19b24e0e05085c1605d1f6cd41086f69e47f8ba9
IRIS-portf <flair465@hotmail.com>
2025-09-22 21:28:38 +0200

    Add menu and improve password change flow and user experience.

--- Login Simulator with loops and user dictionary.py
+++ Login Simulator with loops and user dictionary.py
@@ -14,22 +14,39 @@ while attempts < 3:
     if username in users and password == users[username]:
         print ('Access granted.')
         print ('Great to see you again,', username,'!')
-        change = input('Do you want to change your password? (yes/no): ')
-        if change.lower() == 'yes':
-            while True:
-                new_password = input('Enter new password: ')
-                confirm_password = input('Confirm new password: ')
-                if new_password == confirm_password:
-                    users[username] = new_password
-                    print ('Password updated. Please log in again: ')
-                    attempts = 0
+        while True: #Start menu loop here
+            print ('Here is your menu: ')
+            print ('1. View profile' )
+            print ('2. Change password ')
+            print ('3. Log out')
+            choice = input('Choose an option (1/2/3): ')
+            if choice == '1':
+                print (f'Displays profile of user, {username}.')
+            elif choice == '2':
+                change = input('Do you want to change your password? (yes/no): ')
+                if change.lower() == 'yes':
+                    while True:
+                        new_password = input('Enter new password: ')
+                        confirm_password = input('Confirm new password: ')
+                        if new_password == confirm_password:
+                            users[username] = new_password
+                            print ('Password updated. Please log in again: ')
+                            attempts = 0
+                            logout = True #signal to exit menu
+                            break
+                        else: print ('Passwords do not match. ')
+                    if logout:
+                        break #exit menu loop
+                elif change.lower() == 'no':
+                    print ('No changes made to password.')  
                     break
-            else: print ('Passwords do not match. ')
-        elif change.lower() == 'no':
-            print ('No changes made to password.')  
-            break
-        else:
-            print ('Password not changed.')
+                else:
+                    print ('Password not changed.')
+            elif choice == '3':
+                print ('Logging out...' )
+                break
+            else:
+                print ('Invalid choice. Try again. ')
     else:
         attempts += 1 #counts as failed attempt
         if attempts < 3: #As to not repeat try again after too many attempts
