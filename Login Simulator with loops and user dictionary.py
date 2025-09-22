0ca98a3387ba1e58b9d5ca231eb35be0afc13c37
IRIS-portf <flair465@hotmail.com>
2025-09-22 21:02:17 +0200

    Add password change functionality after successful login.

--- Login Simulator with loops and user dictionary.py
+++ Login Simulator with loops and user dictionary.py
@@ -14,10 +14,26 @@ while attempts < 3:
     if username in users and password == users[username]:
         print ('Access granted.')
         print ('Great to see you again,', username,'!')
-        break
+        change = input('Do you want to change your password? (yes/no): ')
+        if change.lower() == 'yes':
+            while True:
+                new_password = input('Enter new password: ')
+                confirm_password = input('Confirm new password: ')
+                if new_password == confirm_password:
+                    users[username] = new_password
+                    print ('Password updated. Please log in again: ')
+                    attempts = 0
+                    break
+            else: print ('Passwords do not match. ')
+        elif change.lower() == 'no':
+            print ('No changes made to password.')  
+            break
+        else:
+            print ('Password not changed.')
     else:
         attempts += 1 #counts as failed attempt
         if attempts < 3: #As to not repeat try again after too many attempts
             print ('Access denied. Try again.')
 if attempts == 3:
-    print ('Access denied. Too many failed attempts.') 
+    print ('Access denied. Too many failed attempts.')
+     
